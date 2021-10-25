4aba75009bf6887b15690cce2991997385ba2e88
susi1231 <kanisirisusi@gmail.com>
2021-10-25 18:40:06 +0530

    commit

--- MeetMyServiceAutomation/src/main/java/com/meetmyservice/TestBasePackage/TestBase.java
+++ MeetMyServiceAutomation/src/main/java/com/meetmyservice/TestBasePackage/TestBase.java
@@ -2,6 +2,7 @@ package com.meetmyservice.TestBasePackage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -13,15 +14,17 @@ public class TestBase {
 	static WebDriver driver;
 	static Properties prop;
 	
-	public TestBase() throws FileNotFoundException {
+	public TestBase() throws IOException {
 		prop=new Properties();
-		FileInputStream ip=new FileInputStream("C://Users//kalyan//git//repository4//MeetMyServiceAutomation//src//main//java//com//meetmyservice//config/config.properties");
+		FileInputStream ip=new FileInputStream("C:\\Users\\kalyan\\git\\repository4\\MeetMyServiceAutomation\\src\\main\\java\\com\\meetmyservice\\config");
+		prop.load(ip);
 	}
+	///MeetMyServiceAutomation/src/main/java/com/meetmyservice/config/config.properties
 	
 	public static void initialization() {
 		String browserName=prop.getProperty("browser");
 	if (browserName.equals("chrome")) {
-		System.setProperty("webdriver.chrome.driver", "./data/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");;
 		driver=new ChromeDriver();
 	}
 	driver.manage().window().maximize();
@@ -30,6 +33,19 @@ public class TestBase {
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	driver.get(prop.getProperty("url"));
 	}
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	
+	 public static void main(String[] args) throws IOException {
+	 FileInputStream ip=new FileInputStream("C:\\Users\\kalyan\\git\\repository4\\MeetMyServiceAutomation\\src\\main\\java\\com\\meetmyservice\\config\\config.properties");
+	 Properties p=new Properties();
+	 p.load(ip);
+	 String url1=p.getProperty("url1");
+	 WebDriver driver=new ChromeDriver();
+	 driver.get(url1);
+
+	 }
 	
 	/*
 	 * Properties
